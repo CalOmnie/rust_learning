0da3155e559807676d98821dfd9024662f35494d
Louis Groux <louis.cal.groux@gmail.com>
2025-03-01 14:33:41 +0000

    Add logic to use walkdir in python

--- hello_cargo/src/lib.rs
+++ hello_cargo/src/lib.rs
@@ -2,7 +2,9 @@ use pyo3::prelude::*;
 use std::cmp::Ordering;
 use std::io;
 use walkdir::WalkDir;
-
+use walkdir::DirEntry;
+use walkdir::Result;
+use std::sync::Mutex;
 
 #[pyfunction]
 fn walk(d: &str) -> i32 {
@@ -14,6 +16,75 @@ fn walk(d: &str) -> i32 {
     res
 }
 
+#[pyclass]
+struct PyClassIter {
+    count: usize,
+}
+
+#[pymethods]
+impl PyClassIter {
+    #[new]
+    fn new(value: usize) -> Self {
+        PyClassIter {count: value}
+    }
+
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
+
+    fn __next__(&mut self) -> Option<usize> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+
+#[pyclass]
+struct Iter {
+    inner: <WalkDir as IntoIterator>::IntoIter,
+}
+
+#[pymethods]
+impl Iter {
+    #[new]
+    fn new(d: String) -> Iter {
+        Iter {
+            inner: WalkDir::new(d).into_iter()
+        }
+    }
+
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
+
+    fn __next__(mut slf: PyRefMut<'_, Self>) -> Option<String> {
+        let res = slf.inner.next();
+        match res {
+            Some(Ok(a)) => a.into_path().into_os_string().into_string().ok(),
+            Some(Err(a)) => Some(String::from("permission denied")),
+            None => None
+        }
+    }
+}
+
+// #[pyclass]
+// struct Container {
+//     iter: Vec<String>,
+// }
+
+// #[pymethods]
+// impl Container {
+//     fn __iter__(slf: PyRef<'_, Self>) -> PyResult<Py<Iter>> {
+//         let iter = Iter {
+//             inner: slf.iter.clone().into_iter(),
+//         };
+//         Py::new(slf.py(), iter)
+//     }
+// }
 
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
@@ -21,6 +92,7 @@ fn walk(d: &str) -> i32 {
 #[pymodule]
 fn hello_cargo(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(walk, m)?)?;
-
+    m.add_class::<PyClassIter>()?;
+    m.add_class::<Iter>()?;
     Ok(())
 }
--- hello_cargo/test.py
+++ hello_cargo/test.py
@@ -1,26 +1,31 @@
 import os
-from hello_cargo import walk
+import hello_cargo
+print(dir(hello_cargo.hello_cargo))
+from hello_cargo import walk, PyClassIter 
 import time
 
 path = "/"
 
+def find_path(path):
+    for r, dirs, files in os.walk(path):
+        yield r
+        for f in files:
+            yield os.path.join(r, f)
+
+
 start = time.time()
-print(walk(path))
+res = 0
+print(len(list(find_path(path))))
 end = time.time()
 print(f"took {end - start}")
 
 
 start = time.time()
-res = 0
-def walky(path):
-    for r,dirs,files in os.walk(path):
-        for d in dirs:
-            yield os.path.join(r, d)
-        for f in files:
-            yield os.path.join(r, f)
+print(len(list(hello_cargo.Iter(path))))
+print("Took", time.time() - start)
 
 
 
-res = os.walk("/")
-print(next(res))
+# res = os.walk("/")
+# print(next(res))
 
